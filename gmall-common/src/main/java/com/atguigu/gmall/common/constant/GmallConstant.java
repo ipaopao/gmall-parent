9520c90cd592d978dc7a451eaf46514d3de72a15
ipaopao <yangzhiguo@360.com>
2021-01-20 13:13:12 +0800

    添加姓名

--- gmall-common/src/main/java/com/atguigu/gmall/common/constant/GmallConstant.java
+++ gmall-common/src/main/java/com/atguigu/gmall/common/constant/GmallConstant.java
@@ -2,6 +2,6 @@ package com.atguigu.gmall.common.constant;
 
 public class GmallConstant {
 
-    public static final String  KAFKA_TOPIC_STARTUP="GMALL_STARTUP";
-    public static final String  KAFKA_TOPIC_EVENT="GMALL_EVENT";
+    public static final String  KAFKA_TOPIC_STARTUP="";
+    public static final String  KAFKA_TOPIC_EVENT="yangzhiguo";
 }
